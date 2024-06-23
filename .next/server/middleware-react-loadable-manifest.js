2d7b02f1822aaceaf163db86753b950b19df7925
Jarvis Nguyen <jarvisgot@gmail.com>
2024-06-23 21:00:16 +0700

    Update delete home

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST={"index-3.js -> ../src/components/Counter":{"id":"index-3.js -> ../src/components/Counter","files":["static/chunks/src_components_Counter_js.js"]}}
+self.__REACT_LOADABLE_MANIFEST={"index.js -> ../src/components/Counter":{"id":"index.js -> ../src/components/Counter","files":["static/chunks/src_components_Counter_js.js"]}}
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd551e74a37a3ee1"; }
+/******/ 		__webpack_require__.h = function() { return "f55a88853eca4c97"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
